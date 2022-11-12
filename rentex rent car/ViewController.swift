75622a1fdfa52655adc8a8bbfb55555b61dd30b8
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-11-12 19:12:26 -0300

    Initial Commit

---
+++ rentex rent car/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rentex rent car
+//
+//  Created by kenjimaeda on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
